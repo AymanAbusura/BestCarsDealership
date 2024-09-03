c441deeefca4528653b22882eb8016d2113d0ff0
Ayman <aymansalameh1996@hotmail.com>
2024-09-03 05:25:35 -0400

    Adding temporary changes to Github

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,5 +1,9 @@
-# from django.contrib import admin
-# from .models import related models
+from django.contrib import admin
+from .models import CarMake, CarModel
+
+# Registering models for the admin site
+admin.site.register(CarMake)
+admin.site.register(CarModel)
 
 
 # Register your models here.
